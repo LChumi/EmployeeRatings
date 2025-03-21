7cb9b9688a7ef5579d07c7e7324268c906fa98e2
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-03-21 09:00:38 -0500

    feat: added configuration and dependencies into project

--- src/main/java/com/cumpleanos/calificaciones/CalificacionesApplication.java
+++ src/main/java/com/cumpleanos/calificaciones/CalificacionesApplication.java
@@ -1,9 +1,18 @@
 package com.cumpleanos.calificaciones;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Calificaciones", description = "Documentación de calificaciones de colaboradores y empresa API V1 Mongo"
+		)
+)
 public class CalificacionesApplication {
 
 	public static void main(String[] args) {
